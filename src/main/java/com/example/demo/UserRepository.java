90fb89cf31abe5d4c74e289cead676c6864b0bde
qlskjfoqis <sehurn3471@gmail.com>
2025-03-20 21:24:03 +0900

    first commit

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserRepository {
     public String findUser() { // ✅ 오타 수정 (finUser → findUser)
-        return "from Repository";
+        return "User from Repository";
     }
 }
